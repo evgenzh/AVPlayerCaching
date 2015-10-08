d3608598c31c1eb3af9f7b0b01d9db87e490e668
Eugene Zhuk <evgen.zhuk@together.com>
2015-10-08 23:18:16 +0300

    prepared ui hierarchy

---
+++ AVPlayerCaching/AVPlayerCaching/PlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayerViewController.h
+//  AVPlayerCaching
+//
+//  Created by Eugene Zhuk Work on 08.10.15.
+//  Copyright © 2015 Eugene Zhuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerViewController : UIViewController
+@property (nonatomic, copy) NSURL *url;
+@end
--- AVPlayerCaching/AVPlayerCaching/ViewController.m
+++ AVPlayerCaching/AVPlayerCaching/ViewController.m
@@ -7,21 +7,26 @@
 //
 
 #import "ViewController.h"
+#import "PlayerViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, weak) PlayerViewController *playerController;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"playerVC"]) {
+        self.playerController = segue.destinationViewController;
+    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)playURL {
+    
+}
+
+- (IBAction)playPlaylist {
+    
 }
 
 @end
